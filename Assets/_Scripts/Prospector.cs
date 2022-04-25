7e609a5525c534ec192b4921bb41f0e2b791eda3
João Leal <37.joao.leal@gmail.com>
2022-04-25 23:43:09 +0100

    Score managment and display. Background added.

--- Assets/_Scripts/Prospector.cs
+++ Assets/_Scripts/Prospector.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using System;
 
 public class Prospector : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class Prospector : MonoBehaviour
     public float     xOffset = 3;
     public float     yOffset = -2.5f;
     public Vector3   layoutCenter;
+    public Vector2   fsPosMid = new Vector2( 0.5f, 0.90f ); 
+    public Vector2   fsPosRun = new Vector2( 0.5f, 0.75f ); 
+    public Vector2   fsPosMid2 = new Vector2( 0.4f, 1.0f ); 
+    public Vector2   fsPosEnd = new Vector2( 0.5f, 0.95f ); 
 
 
     [Header("Set Dynamically")]
@@ -22,6 +27,7 @@ public class Prospector : MonoBehaviour
     public List<CardProspector> tableau;
     public List<CardProspector> drawPile;
     public List<CardProspector> discardPile;
+    public FloatingScore        fsRun;
 
 
     private void Awake() {
@@ -29,6 +35,8 @@ public class Prospector : MonoBehaviour
     }
 
     private void Start() {
+        Scoreboard.S.score = ScoreManager.SCORE;
+
         deck = GetComponent<Deck>();
         deck.InitDeck(deckXML.text);
         Deck.Shuffle(ref deck.cards);
@@ -192,7 +200,8 @@ public class Prospector : MonoBehaviour
                 MoveToTarget(Draw());
                 UpdateDrawPile();
                 ScoreManager.EVENT(eScoreEvent.draw);
-            
+                FloatingScoreHandler(eScoreEvent.draw);
+
                 break;
 
             case eCardState.tableau:
@@ -214,7 +223,7 @@ public class Prospector : MonoBehaviour
                 MoveToTarget(cd);
                 SetTableauFaces();
                 ScoreManager.EVENT(eScoreEvent.mine);
-
+                FloatingScoreHandler(eScoreEvent.mine);
 
                 break;
         }
@@ -254,15 +263,69 @@ public class Prospector : MonoBehaviour
         if (won)
         {
             ScoreManager.EVENT(eScoreEvent.gameWin);
+            FloatingScoreHandler(eScoreEvent.gameWin);
         }
         else
         {
             ScoreManager.EVENT(eScoreEvent.gameLoss);
+            FloatingScoreHandler(eScoreEvent.gameLoss);
         }
 
         SceneManager.LoadScene("__Prospector_Scene_0");
     }
-    
+
+    private void FloatingScoreHandler(eScoreEvent evt)
+    {
+        List<Vector2> fsPts;
+
+        switch (evt)
+        {
+            case eScoreEvent.draw:
+            case eScoreEvent.gameWin:
+            case eScoreEvent.gameLoss:
+                if (fsRun != null)
+                {
+                    fsPts = new List<Vector2>();
+                    fsPts.Add(fsPosRun);
+                    fsPts.Add(fsPosMid2);
+                    fsPts.Add(fsPosEnd);
+
+                    fsRun.reportFinishTo = Scoreboard.S.gameObject;
+                    fsRun.Init(fsPts, 0, 1);
+                    fsRun.fontSizes = new List<float>(new float[] {28,36,4});
+                    fsRun = null;
+                }
+                break;
+
+            case eScoreEvent.mine:
+                FloatingScore fs;
+
+                Vector2 p0 = Input.mousePosition;
+                p0.x /= Screen.width;
+                p0.y /= Screen.height;
+
+                fsPts = new List<Vector2>();
+                fsPts.Add(p0);
+                fsPts.Add(fsPosMid);
+                fsPts.Add(fsPosRun);
+
+                fs = Scoreboard.S.CreateFloatingScore(ScoreManager.CHAIN, fsPts);
+                fs.fontSizes = new List<float>(new float[] { 4, 50, 28 });
+
+                if (fsRun == null)
+                {
+                    fsRun = fs;
+                    fsRun.reportFinishTo = null;
+                }
+                else
+                {
+                    fs.reportFinishTo = fsRun.gameObject;
+                }
+                
+                break;
+        }
+    }
+
     private void SetTableauFaces()
     {
         foreach (CardProspector cd in tableau)
