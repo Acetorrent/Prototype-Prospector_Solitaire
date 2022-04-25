c6bcc649c8505b84c30e410109c503412f69e68e
João Leal <37.joao.leal@gmail.com>
2022-04-25 02:01:30 +0100

    Card Layers Order

--- Assets/_Scripts/Prospector.cs
+++ Assets/_Scripts/Prospector.cs
@@ -9,11 +9,19 @@ public class Prospector : MonoBehaviour
     [Header("Set in Inspector")]
     public TextAsset deckXML;
     public TextAsset layoutXML;
+    public float     xOffset = 3;
+    public float     yOffset = -2.5f;
+    public Vector3   layoutCenter;
+
 
     [Header("Set Dynamically")]
     public Deck                 deck;
     public Layout               layout;
+    public Transform            layoutAnchor;
+    public CardProspector       target;
+    public List<CardProspector> tableau;
     public List<CardProspector> drawPile;
+    public List<CardProspector> discardPile;
 
     private void Awake() {
         P = this;
@@ -35,7 +43,9 @@ public class Prospector : MonoBehaviour
         layout.ReadLayout(layoutXML.text);
 
         drawPile = ConvertCardsToCardProspectors(deck.cards);
+        LayoutGame();
     }
+
 
     private List<CardProspector> ConvertCardsToCardProspectors(List<Card> cards)
     {
@@ -49,5 +59,40 @@ public class Prospector : MonoBehaviour
         }
 
         return lCP;
+    }
+    CardProspector Draw()
+    {
+        CardProspector cd = drawPile[0];
+        drawPile.RemoveAt(0);
+
+        return cd;
+    }
+
+    private void LayoutGame()
+    {
+        if (layoutAnchor == null)
+        {
+            GameObject tGO = new GameObject("_LayoutAnchor");
+            layoutAnchor = tGO.transform;
+            layoutAnchor.transform.position = layoutCenter;
+        }
+
+        CardProspector cp;
+        foreach (SlotDef tSD in layout.slotDefs)
+        {
+            cp = Draw();
+            cp.faceUp = tSD.faceUp;
+            cp.transform.parent = layoutAnchor;
+            cp.transform.localPosition = new Vector3(layout.multiplier.x * tSD.x,
+                                                     layout.multiplier.y * tSD.y,
+                                                     -tSD.layerID);
+
+            cp.layoutID = tSD.id;
+            cp.slotDef = tSD;
+            cp.state = eCardState.tableau;
+            cp.SetSortingLayerName(tSD.layerName);
+
+            tableau.Add(cp);
+        }
     }
 }
